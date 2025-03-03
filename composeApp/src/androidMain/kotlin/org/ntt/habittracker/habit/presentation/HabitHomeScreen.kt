b9dac61b443922be13553c7071ca15c71f298cc8
Yousif Beeai <267209@nttdata.com>
2025-03-03 10:40:39 -0500

    Android: added and called app bar within HabitHomeScreen, updated UI test for test coverage

--- composeApp/src/androidMain/kotlin/org/ntt/habittracker/habit/presentation/HabitHomeScreen.kt
+++ composeApp/src/androidMain/kotlin/org/ntt/habittracker/habit/presentation/HabitHomeScreen.kt
@@ -1,18 +1,24 @@
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import org.ntt.habittracker.core.presentation.AppBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HabitHomeScreen() {
-	Column(
-		modifier = Modifier.fillMaxSize(),
-		horizontalAlignment = Alignment.CenterHorizontally,
-		verticalArrangement = Arrangement.Center
-	) {
-		Text("Hello World!")
+	val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+	Scaffold(
+		modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+		topBar = { AppBar() }
+	) { innerPadding ->
+		androidx.compose.material3.Text(
+			modifier = Modifier.padding(innerPadding),
+			text = "List Tiles Placeholder"
+		)
 	}
 }
