83996ac0d6ba53c159511822b2ae937e3c1dd845
Shawn Antonucci <266512@nttdata.com>
2025-03-06 11:45:13 -0500

    HT-39 IOS habitcard plus testing

--- iosApp/iosApp/core/presentation/iOSCheckboxToggleStyle.swift
+++ iosApp/iosApp/core/presentation/iOSCheckboxToggleStyle.swift
@@ -4,19 +4,22 @@ import SwiftUI
 
 struct iOSCheckboxToggleStyle: ToggleStyle {
     func makeBody(configuration: Configuration) -> some View {
-        // 1
         Button(action: {
-
-            // 2
             configuration.isOn.toggle()
-
         }, label: {
             HStack {
-                // 3
                 Image(systemName: configuration.isOn ? "checkmark.square" : "square")
-
                 configuration.label
             }
-        })
+        }).buttonStyle(NoTapAnimationStyle())
+    }
+}
+
+struct NoTapAnimationStyle: PrimitiveButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+        // Make the whole button surface tappable. Without this only content in the label is tappable and not whitespace. Order is important so add it before the tap gesture
+            .contentShape(Rectangle())
+            .onTapGesture(perform: configuration.trigger)
     }
 }
--- iosApp/iosApp/habit/presentation/HabitCard.swift
+++ iosApp/iosApp/habit/presentation/HabitCard.swift
@@ -4,19 +4,28 @@ struct HabitCard: View {
     @State private var isOn = false
     
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 10)
-                .fill(.white)
-                .shadow(radius: 10)
+        ZStack(alignment: Alignment.leading) {
+            RoundedRectangle(cornerRadius: 15)
+                .fill(.gray)
+                .opacity(0.3)
+                .shadow(radius: 5)
             
-            HStack{
-                Toggle(isOn: $isOn) {
-                            Text("Habit Text")
-                        }
-                .toggleStyle(iOSCheckboxToggleStyle())
+            Toggle(isOn: $isOn) {
+                Text("Habit Text")
+                    .accessibilityIdentifier("HabitCardText")
             }
+            .toggleStyle(iOSCheckboxToggleStyle())
+                .padding()
+                .font(.title2)
+                .foregroundColor(.black)
+                .accessibilityIdentifier("HabitCardToggle")
         }
-        .frame(width: 350, height: 100)
+        .frame(width: 350, height: 75)
     }
 }
 
+
+//#Preview
+//{
+//    HabitCard()
+//}
