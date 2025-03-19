effeabcc8f0c1987b0b222c7b41ea8f1314ae341
Shawn Antonucci <266512@nttdata.com>
2025-03-19 10:51:23 -0400

    HT-45 fix for IOS

--- shared/src/commonMain/kotlin/org/ntt/habittracker/database/HabitsDatabase.kt
+++ shared/src/commonMain/kotlin/org/ntt/habittracker/database/HabitsDatabase.kt
@@ -1,10 +1,18 @@
 package org.ntt.habittracker.database
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
 
 @Database(entities = [Habit::class], version = 1)
+@ConstructedBy(HabitsDatabaseConstructor::class)
 abstract class HabitsDatabase : RoomDatabase() {
     abstract fun getHabitsDao(): HabitsDao
 }
 
+// The Room compiler generates the `actual` implementations.
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object HabitsDatabaseConstructor : RoomDatabaseConstructor<HabitsDatabase> {
+    override fun initialize(): HabitsDatabase
+}
